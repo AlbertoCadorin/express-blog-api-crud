e767591fdc7adb68d1a00ee75819838239725c99
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-28 14:00:04 +0100

    feat: add destroy controller

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -38,18 +38,21 @@ function modify(req,res) {
 }
 
 function destroy(req,res) {
-    const id = parseInt(req.param.id)
+    const id = parseInt(req.params.id)
     const postId = postsList.find( postId => postId.id === id)
   
     if (!postId){
       res.status(404)
+
       return res.json({
         error :"Not Found",
         message : "Post non trovato"
         
       })
     }
-    res.json(postId) 
+    postsList.splice(postsList.indexOf(postId),1)
+    res.sendStatus(204) 
+    console.log(postsList)
 }
 
 module.exports = {index,show,store,update,modify,destroy}
